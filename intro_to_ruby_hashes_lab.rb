a156f2652068a3d1f0caac82a37c8e48138bf6e4
Lisa Jung <lisahjung@gmail.com>
2020-03-06 22:49:58 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,11 @@ def new_hash
 end
 
 def my_hash
-  my_hash = {Lisa:"John"}
+  my_hash = {"groceries":"eggs"}
 end
 
 def pioneer
   pioneer= {:name=>"Grace Hopper"}
-  return pioneer
 end
 
 def id_generator
@@ -18,7 +17,7 @@ end
 def my_hash_creator(key, value)
   hash_creator = { }
   hash_creator[key] = value
-  return hash_creator
+  hash_creator
 end
 
 def read_from_hash(hash, key)
